10eb838091777bf784e6e04f84e854bd516b2cca
Aalturki <Aalturki@sure.com.sa>
2025-07-21 14:27:25 +0300

    editcomment

--- StudentsProjectAPI/Models/Department.cs
+++ StudentsProjectAPI/Models/Department.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        //navigational
+        //navigational5
         public ICollection<Student> Students { get; set; }
     }
 }
